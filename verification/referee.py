872a8364675d9199e58c85ed55c6d4775f15f903
Valentin Bryukhanov <bvv.mag@gmail.com>
2014-04-21 15:22:07 +1200

    new tests

--- verification/referee.py
+++ verification/referee.py
@@ -2,8 +2,19 @@ from checkio.signals import ON_CONNECT
 from checkio import api
 from checkio.referees.io import CheckiOReferee
 
+
+cover = """def cover(func, in_data):
+    return func(tuple(tuple(row) for row in in_data))
+"""
+
 from tests import TESTS
 
 api.add_listener(
     ON_CONNECT,
-    CheckiOReferee(tests=TESTS).on_ready)
+    CheckiOReferee(tests=TESTS,
+                   cover_code={
+                       'python-27': cover,  # or None
+                       'python-3': cover
+                   },
+                   function_name="count_gold"
+    ).on_ready)
